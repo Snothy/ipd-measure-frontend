252fdafec9031a5221db5a16760c9e80173036bb
drumevp <drumevp@coventry.ac.uk>
2021-06-23 21:50:55 +0300

    Add user guidance and automatic taking of photo
    The user is now dynamically guided as to how close they need to be to the camera and once certain conditions (such as open eyes, not too close or far, correct part of frame) are met, the picture is taken after a 3 second countdown.

--- components/uploader.js
+++ components/uploader.js
@@ -13,17 +13,26 @@ class Uploader extends React.Component {
             type: null,             //which camera is currently in use
             faceData: [],           //The data about the face collected from the Google Mobile Vision framework
             faceDetected: false,    //whether a face is detected in frame
-            photo_style: {
-                position: 'relative',
-                width: 480,
-                height: 480
-            },
-            //smiling: false          //whether the subject is smiling
-            eyesOpen: true         //whether the subject's eyes are open (they need to be)
+            eyesOpen: true,         //whether the subject's eyes are open (they need to be)
+            faceTooClose: false,
+            faceTooFar: false,
+            faceRecenter: false,
+            ready: false, //red-#FF0000  green-#00FF00
+            countdownInitiated: false,
+            countdownTime: 3,       //2 seconds before a picture is taken
+
+
+
         }
         this.handleFacesDetected = this.handleFacesDetected.bind(this); //the handler needs access to state
         this.faceSquare = this.faceSquare.bind(this);
+        this.startCountdown = this.startCountdown.bind(this);
+        this.handleCountdown = this.handleCountdown.bind(this);
+        this.cancelCountdown = this.cancelCountdown.bind(this);
+        this.takePicture = this.takePicture.bind(this);
     }
+
+    countdownTimer = null;
 
     async componentDidMount() {
         this.setState({ hasPermission: null });
@@ -44,33 +53,89 @@ class Uploader extends React.Component {
             this.setState({ faceDetected: true });
             this.setState({ faceData: faces });
 
+            //Make sure they subject's face at the correct distance (they fit the square parameters)
+            const currLength = faces[0].bounds.size.width; //its a square so the 2 sides are the same length
+            const currY = faces[0].bounds.origin.y;        //even if the square is the same size, they might not be in the correct position of the frame
+            const currX = faces[0].bounds.origin.x;
+            if(currLength < styles.minStyle.width) {
+                this.setState({ faceTooFar: true });
+                this.setState({ ready: false });
+            } else {
+                this.setState({ faceTooFar: false });
+            }
+            if(currLength > styles.maxStyle.width ) {
+                this.setState({ faceTooClose: true });
+                this.setState({ ready: false });
+            } else {
+                this.setState({ faceTooClose: false });
+            }
+            if(currY > styles.minSquare.top || currX > styles.minSquare.left) {
+                this.setState({ faceRecenter: true });
+                this.setState({ ready: false });
+            } else {
+                this.setState({ faceRecenter: false })
+            }
+
             //Check whether their eyes are open
             if(faces[0].leftEyeOpenProbability > 75/100 && faces[0].rightEyeOpenProbability > 60/100) {
                 this.setState({ eyesOpen: true });
                 //execute code for countdown timer
-
+                if( !(this.state.faceTooFar || this.state.faceTooClose || this.state.faceRecenter || this.state.countdownInitiated) ) { //if theyre all false => we start the countdown
+                    this.setState({ ready: true }); 
+                    this.startCountdown();
+                    //console.log('passed through');
+                }
             } else {
                 this.setState({ eyesOpen: false });
+                this.setState({ ready: false });
             }
-
-            /*
-            //this was for fun / experimenting
-            //console.log(faces[0].smilingProbability);
-            if(faces[0].smilingProbability > 60/100) {
-                this.setState({ smiling: true });
-            } else {
-                this.setState({ smiling: false });
-            }
-            */
 
         } else {
             this.setState({ faceDetected: false });
             this.setState({ eyesOpen: true }); //if no faces are detected, there's nobody to open their eyes
-            this.setState({ faceData: [] })
+            this.setState({ faceData: [] });
+            this.setState({ faceTooClose: false });
+            this.setState({ faceTooFar: false });
+            this.setState({ faceRecenter: false });
+            this.setState({ ready: false });
             //this.setState({ smiling: false });
         } 
         //TODO: handle exception where there are multiple faces detected
         //maybe force the user to only include only one person(face) in frame
+    }
+
+
+    startCountdown() {
+        this.setState({ countdownInitiated: true });
+        this.countdownTimer = setInterval(this.handleCountdown, 1000);
+    }
+
+    handleCountdown() {
+        if(this.state.countdownTime > 0) {
+            if(!this.state.ready) {
+                return this.cancelCountdown();
+            }
+            let updatedTime = this.state.countdownTime - 1;
+            this.setState({ countdownTime: updatedTime });
+        } else {
+            this.cancelCountdown();
+            //console.log(this.state.countdownTime);
+            this.takePicture();
+        }
+    }
+
+    cancelCountdown() {
+        clearInterval(this.countdownTimer);
+        this.setState({ countdownTime: 3, countdownInitiated: false });
+    }
+
+    takePicture() {
+        if(this.camera) {
+            //let photo = await this.camera.takePictureAsync();
+            console.log("photo taken");
+            //console.log(this.state.countdownTime);
+            //make post request to backend
+        }
     }
 
 
@@ -83,17 +148,24 @@ class Uploader extends React.Component {
                 top: this.state.faceData[0].bounds.origin.y,
                 left: this.state.faceData[0].bounds.origin.x,
             };
-
+            let squareColour;
+            if(this.state.ready) {
+                squareColour = '#00FF00'; //green
+            } else {
+                squareColour = '#FF0000'; //red
+            }
             const style = {
                 width: this.state.faceData[0].bounds.size.width,
                 height: this.state.faceData[0].bounds.size.height,
                 borderWidth: 2,
-                borderColor: '#000000'
+                borderColor: squareColour
             };
 
             const textColor = {
                 color: '#ffffff'
             };
+
+            //console.log(square.top, square.left, style.width, style.height); //16-25-left   | 68-75-top
 
             return (
                 <>
@@ -127,14 +199,15 @@ class Uploader extends React.Component {
         return (
             <>
                 <Camera style={styles.camera} type={this.state.type}
-                
+                ref={ref => {
+                    this.camera = ref;}}
                 onFacesDetected={this.handleFacesDetected}
                 faceDetectorSettings={{
-                  mode: FaceDetector.Constants.Mode.accurate,
-                  detectLandmarks: FaceDetector.Constants.Landmarks.all,
-                  runClassifications: FaceDetector.Constants.Classifications.all,
-                  minDetectionInterval: 100,
-                  tracking: true
+                    mode: FaceDetector.Constants.Mode.accurate,
+                    detectLandmarks: FaceDetector.Constants.Landmarks.all,
+                    runClassifications: FaceDetector.Constants.Classifications.all,
+                    minDetectionInterval: 100,
+                    tracking: true
                 }}
                 >
                     <View style={styles.buttonContainer}>
@@ -153,21 +226,59 @@ class Uploader extends React.Component {
                             
                     </TouchableOpacity>
 
-                    <View>
-                        { this.faceSquare .call(this) }
+                         <View>
+                            { this.faceSquare .call(this) }
                         </View>
-                    <TouchableOpacity>
 
-                    </TouchableOpacity>
+
+
+
+
+
+                        <View
+                        style={styles.textStyle}>
+                            {this.state.ready 
+                            ? <Text style={styles.countdown}>
+                            {this.state.countdownTime}  
+                            </Text>
+                            : <Text></Text>
+                            }
+                        </View>
+
+
+
+    
+                    <View style={styles.warningTextStyle}>
+                        {this.state.faceDetected
+                        ? <Text ></Text>
+                        : <Text style={styles.warningText}>No face detected.</Text>}
+                        
+                        {this.state.eyesOpen
+                        ? <Text></Text>
+                        : <Text style={styles.warningText}>Please open your eyes.</Text>}
+
+                        {this.state.faceTooClose
+                        ? <Text style={styles.warningText}>Move back.</Text>
+                        : <Text></Text>}
+
+                        {this.state.faceTooFar
+                        ? <Text style={styles.warningText}>Move closer.</Text>
+                        : <Text></Text>}
+
+                        {this.state.faceRecenter
+                        ? <Text style={styles.warningText}>Recenter.</Text>
+                        : <Text></Text>}
+                    </View>
+
+
+
+
+
+
                     </View>
                 </Camera>
-                {this.state.faceDetected
-                ? <Text></Text>
-                : <Text>No face detected.</Text>}
-                
-                {this.state.eyesOpen
-                ? <Text></Text>
-                : <Text>Please open your eyes.</Text>}
+
+            
             </>
         );
     }
@@ -186,6 +297,50 @@ const styles = StyleSheet.create({
     camera: {
         width: 300,
         height: 400,
+    },
+    minSquare: {
+        position: 'absolute',
+        //position of the top left corner of a square - from the api
+        top: 70,
+        left: 30,
+    },
+    minStyle: {
+        width: 240,
+        height: 240,
+        borderWidth: 2,
+        borderColor: '#000000'
+    },
+    maxSquare: {
+        position: 'absolute',
+        //position of the top left corner of a square - from the api
+        top: 50,
+        left: 13,
+    },
+    maxStyle: {
+        width: 276,
+        height: 276,
+        borderWidth: 2,
+        borderColor: '#000000'
+    },
+    textStyle: {
+        flex: 1,
+        flexDirection: 'row',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    countdown: {
+        fontSize: 40,
+        color: 'white'
+    },
+    warningTextStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    warningText: {
+        fontSize: 20,
+        color: 'white'
     },
     buttonContainer: {
 
