2f0549c8a4f414324974df1b7392b9ff6c9c0d1b
drumevp <drumevp@coventry.ac.uk>
2021-06-23 19:17:27 +0300

    Add dynamic box that tracks the face
    Box that dynamically tracks & surrounds the subject's face. This will be used to determine & direct how far from the camera they should be.

--- components/uploader.js
+++ components/uploader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import {Button} from 'antd-mobile-rn';
 import { Camera } from 'expo-camera';
 import * as FaceDetector from 'expo-face-detector';
@@ -11,10 +11,18 @@ class Uploader extends React.Component {
         this.state = {
             hasPermission: null,    //whether the application has permission to access the device's camera
             type: null,             //which camera is currently in use
+            faceData: [],           //The data about the face collected from the Google Mobile Vision framework
             faceDetected: false,    //whether a face is detected in frame
-            smiling: false          //whether the subject is smiling
+            photo_style: {
+                position: 'relative',
+                width: 480,
+                height: 480
+            },
+            //smiling: false          //whether the subject is smiling
+            eyesOpen: true         //whether the subject's eyes are open (they need to be)
         }
         this.handleFacesDetected = this.handleFacesDetected.bind(this); //the handler needs access to state
+        this.faceSquare = this.faceSquare.bind(this);
     }
 
     async componentDidMount() {
@@ -25,23 +33,78 @@ class Uploader extends React.Component {
         this.setState({ hasPermission: status === 'granted' });
     }
 
-
+    /**
+     * This function updates the state upon detecting a face in frame
+     * @param {faces} - Array of objects with data about the faces inside the frame (camera frame)
+     */
     handleFacesDetected({faces}) {
         //dynamically updating the state depending on whether there is a face detected in frame
-        if(faces.length > 0) {
+        if(faces.length === 1) {
+        //Only working with 1 face at a time
             this.setState({ faceDetected: true });
+            this.setState({ faceData: faces });
+
+            //Check whether their eyes are open
+            if(faces[0].leftEyeOpenProbability > 75/100 && faces[0].rightEyeOpenProbability > 60/100) {
+                this.setState({ eyesOpen: true });
+                //execute code for countdown timer
+
+            } else {
+                this.setState({ eyesOpen: false });
+            }
+
+            /*
+            //this was for fun / experimenting
             //console.log(faces[0].smilingProbability);
             if(faces[0].smilingProbability > 60/100) {
                 this.setState({ smiling: true });
             } else {
                 this.setState({ smiling: false });
             }
+            */
+
         } else {
             this.setState({ faceDetected: false });
-            this.setState({ smiling: false });
-        }
-
+            this.setState({ eyesOpen: true }); //if no faces are detected, there's nobody to open their eyes
+            this.setState({ faceData: [] })
+            //this.setState({ smiling: false });
+        } 
+        //TODO: handle exception where there are multiple faces detected
+        //maybe force the user to only include only one person(face) in frame
     }
+
+
+    faceSquare() {
+        //console.log(this.state.faceData.length);
+        if(this.state.faceData.length > 0) {
+            const square = {
+                position: 'absolute',
+                //position of the top left corner of a square - from the api
+                top: this.state.faceData[0].bounds.origin.y,
+                left: this.state.faceData[0].bounds.origin.x,
+            };
+
+            const style = {
+                width: this.state.faceData[0].bounds.size.width,
+                height: this.state.faceData[0].bounds.size.height,
+                borderWidth: 2,
+                borderColor: '#000000'
+            };
+
+            const textColor = {
+                color: '#ffffff'
+            };
+
+            return (
+                <>
+                <View style = {square}>
+                    <View style = {style}></View>
+                </View>
+                </>
+            )
+        }
+    }
+
 
     componentDidUpdate() {   
     }
@@ -55,7 +118,12 @@ class Uploader extends React.Component {
         if (this.state.hasPermission === false) {
         return <Text>No access to camera</Text>;
         }
-    
+
+
+
+
+
+
         return (
             <>
                 <Camera style={styles.camera} type={this.state.type}
@@ -81,19 +149,25 @@ class Uploader extends React.Component {
                             });
                         }}>
                         <Text style={styles.text}> Flip </Text>
+
+                            
+                    </TouchableOpacity>
+
+                    <View>
+                        { this.faceSquare .call(this) }
+                        </View>
+                    <TouchableOpacity>
+
                     </TouchableOpacity>
                     </View>
                 </Camera>
-                <Button type="primary" htmlType="submit">
-                    Test button
-                </Button>
                 {this.state.faceDetected
-                ? <Text>DETECTED FACE</Text>
-                : <Text>NOT DETECTED</Text>}
-
-                {this.state.smiling
-                ? <Text>SMILING</Text>
-                : <Text>NOT SMILING</Text>}
+                ? <Text></Text>
+                : <Text>No face detected.</Text>}
+                
+                {this.state.eyesOpen
+                ? <Text></Text>
+                : <Text>Please open your eyes.</Text>}
             </>
         );
     }
@@ -112,6 +186,12 @@ const styles = StyleSheet.create({
     camera: {
         width: 300,
         height: 400,
+    },
+    buttonContainer: {
+
+    },
+    button: {
+
     }
   });
 
